23e3a4179c805845283e6533dec9c0932fe3dab5
Jessica Kiroff <jesskiroff@gmail.com>
2024-01-29 15:11:45 -0500

    code works

---
+++ rps.py
@@ -0,0 +1,60 @@
+rock = "rock"
+r = '''
+  _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = "paper"
+p = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = "scissors"
+s = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+
+import random
+
+user = input("Which do you play? Rock, Paper or Scissors? ")
+users_choice = user.lower()
+if users_choice == "rock":
+    print(rock + r)
+elif users_choice == "paper":
+    print(paper + p)
+else:
+    print(scissors + s)
+rps = [rock, paper, scissors]
+rps_comp = len(rps)
+random_choice = random.randint(0, rps_comp - 1)
+computer_output = rps[random_choice]
+comp_output_str = str(computer_output)
+
+print(users_choice)
+print(comp_output_str)
+if users_choice == comp_output_str:
+    print("Tie game")
+elif users_choice == "rock" and comp_output_str == "paper":
+    print("You lose")
+elif users_choice == "paper" and comp_output_str == "scissors":
+    print("You lose")
+elif users_choice == "scissors" and comp_output_str == "rock":
+    print("You lose")
+else:
+    print("You win!")
